0e22657110669de4e97b1fadcf1fddf7b37a81be
Marshawn-67 <marshawn.marchall@hgs.hiddengeniusproject.orgit config --global user.email marshawn.marchall@hgs.hiddengeniusproject.org>
2025-06-24 14:05:38 -0700

    Exit ticket

---
+++ Python/Week2/QuikMath.py
@@ -0,0 +1,9 @@
+#1. Declare and Assign 2 variables
+num1 =-77
+num2 =7
+
+#2. Declare and assign a 3rd variable that is the sum of the first 2
+num3 = num1 + num2
+
+# 3. print the 3rd variable to the terminal
+print(num3)
