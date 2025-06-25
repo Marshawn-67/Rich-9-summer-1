023640d8d03e40ec58ef882b691794388968ad81
Marshawn-67 <marshawn.marchall@hgs.hiddengeniusproject.orgit config --global user.email marshawn.marchall@hgs.hiddengeniusproject.org>
2025-06-25 13:43:03 -0700

    UserInput.md

---
+++ Python/Week2/UserInput.md
@@ -0,0 +1,3 @@
+We used name = input to get answers from the user
+string
+When asking for the users age that has to have a user input
--- Python/Week2/UserInput.py
+++ Python/Week2/UserInput.py
@@ -1,2 +1,6 @@
 name = input("hi, I am python! what is your name?")
 print("Hello, " + name +  ", its nice to meet you!")
+age = input ("How old are you?")
+print("that's cool I'm 35!")
+Grade = input ("What grade are you in?")
+print("That,s cool!")
