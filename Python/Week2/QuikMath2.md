014ff82a2bcf6606769397f4712970c21e28a76b
Marshawn-67 <marshawn.marchall@hgs.hiddengeniusproject.orgit config --global user.email marshawn.marchall@hgs.hiddengeniusproject.org>
2025-06-30 11:32:06 -0700

    QuikMath2 exit ticket

---
+++ Python/Week2/QuikMath2.md
@@ -0,0 +1,1 @@
+A funtion is when the code 
---
+++ Python/Week2/QuikMath2.py
@@ -0,0 +1,16 @@
+def Add(addend1, addend2):
+    return addend1 + addend2 
+
+sum = Add(5, 35) 
+print(sum)
+
+def divide(addend1, addend2):
+    return addend1 / addend2
+sum = divide(5, 35)
+
+divide(5, 35)
+
+print(sum)
+
+def substract ()
+    return
