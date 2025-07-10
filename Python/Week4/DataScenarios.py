c74d153b1da4c8251dd249752485af68aceb8c35
Marshawn-67 <marshawn.marchall@hgs.hiddengeniusproject.orgit config --global user.email marshawn.marchall@hgs.hiddengeniusproject.org>
2025-07-10 14:30:13 -0700

    Work done

---
+++ Python/Week4/DataScenarios.py
@@ -0,0 +1,15 @@
+print("Scenario #1: A restarurant menu with prices for each item")
+print("best Structure: Dictinary; best to pair food with price in key/value")
+menu = {
+     "French Toast": 12,
+     "Grand Slam":   12,
+     "T-Bone Steak": 18,
+     "Acacado Toast": 15,
+}
+for key, item in menu.items():
+    print(key, ":", item)
+
+    print("Scenario 2#: high scores to an arcade game")
+
+
+    
