610693604052597ef9b6d6c0c0b8bb4891465587
Marshawn-67 <marshawn.marchall@hgs.hiddengeniusproject.orgit config --global user.email marshawn.marchall@hgs.hiddengeniusproject.org>
2025-07-14 14:13:57 -0700

    Work done

---
+++ Python/Week5/QuickMath3.md
@@ -0,0 +1,5 @@
+Define what Object Oriented Programming is in your own words:a program that helps with objects 
+ How many pillars are there in OOP? Name all of them.:there are 4 pillers named Abstraction,Encapsulation,Inheritance and Polymorphism.
+What are the main parts of a class?:instance variables and constructors
+ How do we create a class in Python?: using the class keyword
+What is the difference between a class and an object?:a class is a blueprint and an object is a spcififc section of that class.
---
+++ Python/Week5/QuickMath3.py
@@ -0,0 +1,23 @@
+class calculator:
+    def Add(self, x, y):
+        return x + y 
+    
+    def Subtract(self, x, y):
+        return x - y 
+    
+    def Multiply(self, x, y):
+        return x * y 
+    
+    def Divide(self, x, y):
+        return x / y 
+    
+    calculator = calculator()
+
+    sum = calculator.Add(25, 78)
+    difference = calculator.Subtract(10, 5)
+    product = calculator.Multiply(5, 8)
+    Dividend = calculator.Divide(12, 4)
+print(sum)
+print(difference)
+print(product)
+print(Dividend)
